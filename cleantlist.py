12750ebbe8194ecd12feadf4a53c68b83b282f7f
Leonidus <72128281+Leonidus0x10@users.noreply.github.com>
2020-12-06 13:55:32 -0600

    Update cleantlist.py

--- cleantlist.py
+++ cleantlist.py
@@ -3,13 +3,14 @@
 # ProxyBrokerParse
 # Written by Leonidus
 # Usage: python3 cleanlist.py outfile.txt
+# this is meant for http proxy list ive yet to implement socks5 and https
 import sys
 prox_list = sys.argv[1]
 f = open(prox_list, "r")
 proxies = f.readlines()
 
 for proxy in proxies:
-    x_proxy = proxy.split(" ")[5]
+    x_proxy = proxy.split(" ")[5] # change this to sort different outputs
     new_proxy = x_proxy.replace(">", "")
     new_list = open("new_list.txt", "a")
     new_list.write(new_proxy)
